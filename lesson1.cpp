cf8648d6c8bd1a6554aa1f118bd83ecf4a848683
Vader <manavhirani@gmail.com>
2023-03-31 18:18:42 -0400

    v2

---
+++ lesson1.cpp
@@ -0,0 +1,12 @@
+// Let's start with the classic 'Hello World' C++ program
+
+#include <iostream>
+// This line includes the iostream library, which allows the program to input and output data. 
+// The angle brackets (<>) indicate that the library is a system library and is located in the 
+// standard library directory.
+
+
+
+int main() {
+    std::cout << "Hello, World!\n";
+};
